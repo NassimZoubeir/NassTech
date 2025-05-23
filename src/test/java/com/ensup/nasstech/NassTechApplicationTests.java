c961b629ab09d2a049c3e7353cf9b558b45acc37
Nassim <nassimz@outlook.fr>
2025-05-23 14:34:14 +0200

    changement de la bdd

--- src/test/java/com/ensup/nasstech/NassTechApplicationTests.java
+++ src/test/java/com/ensup/nasstech/NassTechApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.boulange;
+package com.ensup.nasstech;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BoulangeApplicationTests {
+class NassTechApplicationTests {
 
 	@Test
 	void contextLoads() {
